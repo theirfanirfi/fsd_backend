653a27ba0a63c71aaf78153a5b1e741671c86c46
Irfan Ullah <theirfanullah@gmail.com>
2025-05-30 17:42:57 +0500

    express server endpoints

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express'
+
+
+const app = express();
+const port = 5000
+
+import students from './data/students.js'
+
+
+
+app.get('/students', function(req,res,next){
+    res.json(students);
+})
+
+app.get('/students/:id', (req, res, next) => {
+    let id = req.params.id;
+    let student = students.find(student => student.id == id)
+    res.json(student);
+})
+
+app.get('/students/:id/:sign', (req, res, next)=> {
+    let {id, sign} = req.params
+
+    let filtered_students = []
+    if(sign == "lt"){
+        filtered_students = students.filter(student => student.id < id)
+    }else {
+        filtered_students = students.filter(student => student.id > id)
+    }
+
+
+    res.json(filtered_students)
+})
+
+
+app.listen(port,()=> {
+console.log('app is running');
+});
