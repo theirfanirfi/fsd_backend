fd01b5b019907499d1b06ffdfff2fe6d0d9455e7
Nomikhan123786 <nomank7887682@gmail.com>
2025-05-31 18:28:01 +0500

    changes

--- index.js
+++ index.js
@@ -1,72 +1,10 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
-
-const app = express();
+const app = express()
 const port = 5000
+        
 
-import students from './data/students.js'
+import student_router from './Api/student_api.js'
 
-
-
-app.get('/students', function(req,res,next){
-    res.json(students);
-})
-
-app.get('/students/:id', (req, res, next) => {
-    let id = req.params.id;
-    let student = students.find(student => student.id == id)
-    res.json(student);
-})
-
-app.get('/students/:id/:sign', (req, res, next)=> {
-    let {id, sign} = req.params
-
-    let filtered_students = []
-    if(sign == "lt"){
-        filtered_students = students.filter(student => student.id < id)
-    }else {
-        filtered_students = students.filter(student => student.id > id)
-    }
-
-
-    res.json(filtered_students)
-})
-
-app.get('/students/:id/:sign/:gender', (req, res, next)=> {
-    let {id, sign,gender} = req.params
-
-    let students_gender= []
-    if(sign == "lt"){
-        students_gender = students.filter(student => student.id < id)
-    }else {
-        students_gender= students.filter(student => student.id > id)
-    }
-
-
-    if (gender === "M") {
-        students_gender = students_gender.filter(student => student.gender === "Male");
-    } else {
-        students_gender = students_gender.filter(student => student.gender === "Female");
-    }
-
-
-    res.json(students_gender)
-})
-app.get('/students/:id/:sign/:gender', (req, res, next)=> {
-    let {id, sign,gender} = req.params
-
-    let filtered_gender_students = []
-    if(sign=="lt"&& gender=="male"){
-        filtered_gender_students=students.filter(student=>student.id>id && student.gender=="Male")
-    }
-    else{
-        filtered_gender_students=students.filter(student=>student.id<id && student.gender=="Female")
-    }
-    res.json(filtered_gender_students)
-}) 
-
-
-
-app.listen(port,()=> {
-console.log('app is running');
-});
+app.use('/students',student_router)
